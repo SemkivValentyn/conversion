a38bddc899a02a51c61b55b9da385e7c4470ec49
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-10-15 01:38:14 +0300

    state 3

--- src/components/сurrentExchangeRate/CurrentExchangeRate.js
+++ src/components/сurrentExchangeRate/CurrentExchangeRate.js
@@ -7,54 +7,39 @@ import './currentExchangeRate.scss'
 
 const CurrentExchangeRate = () => {
     const [loading, setLoading] = useState(true);
-    const [usdtoUah, setUsdToUah] = useState(null);
-    const [eurotoUah, setEuroToUah] = useState(null);
+    const [currencies, setCurrencies] = useState([]);
+
+    const currenciesToShow = ['USD', 'EUR'];
 
     useEffect(() => {
         getExchangeRate()
             .then((data) => {
-                const usd = data.find((currency) => {
-                    return currency.cc === 'USD';
-                });
-
-                const euro = data.find((currency) => {
-                    return currency.cc === 'EUR';
-                });
-
-                setUsdToUah(usd);
-                setEuroToUah(euro);
+                setCurrencies(data.filter((currency) => {
+                    return currenciesToShow.indexOf(currency.cc) > -1;
+                }));
 
                 setLoading(false)
             });
-
-        // If get exchange rate request fails?
-        // if get exchange rate has no USD or Eur?
-
-        // eslint-disable-next-line
     }, [])
-
 
     if (loading) {
         return "Loading..."
     }
 
-    return (usdtoUah && eurotoUah) ? (
+    return currencies.length ? (
         <div className="currencies">
-
-            <Currency
-                imgUrl="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
-                name={usdtoUah.txt}
-                rate={usdtoUah.rate}
-            />
-            <hr />
-            <Currency
-                imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/800px-Flag_of_Europe.svg.png?20081021155534"
-                name={eurotoUah.txt}
-                rate={eurotoUah.rate}
-            />
+            {
+                currencies.map((currency) => (
+                    <Currency
+                        key={currency.r030}
+                        imgUrl={`/flags/${currency.cc.toLowerCase()}.svg`}
+                        name={currency.txt}
+                        rate={currency.rate}
+                        code={currency.cc}
+                    />
+                ))
+            }
         </div>
-
-
     ) : (<p>No data</p >);
 
 }
