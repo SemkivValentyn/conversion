ed008dd0f77e0f85f051126fad2baa2f11270bad
Valentyn Semkiv <74613837+SemkivValentyn@users.noreply.github.com>
2022-10-16 19:30:50 +0300

    finish

--- src/components/сurrentExchangeRate/CurrentExchangeRate.js
+++ src/components/сurrentExchangeRate/CurrentExchangeRate.js
@@ -1,47 +1,47 @@
 import Currency from "../currency/Currency";
 
 import { useState, useEffect } from "react";
-import { getExchangeRate } from '../../services/ExchangeRateServices';
+import { getExchangeRate } from "../../services/ExchangeRateServices";
 
-import './currentExchangeRate.scss'
+import "./currentExchangeRate.scss";
 
 const CurrentExchangeRate = () => {
-    const [loading, setLoading] = useState(true);
-    const [currencies, setCurrencies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currencies, setCurrencies] = useState([]);
 
-    const currenciesToShow = ['USD', 'EUR'];
+  const currenciesToShow = ["USD", "EUR"];
 
-    useEffect(() => {
-        getExchangeRate()
-            .then((data) => {
-                setCurrencies(data.filter((currency) => {
-                    return currenciesToShow.indexOf(currency.cc) > -1;
-                }));
+  useEffect(() => {
+    getExchangeRate().then((data) => {
+      setCurrencies(
+        data.filter((currency) => {
+          return currenciesToShow.indexOf(currency.cc) > -1;
+        })
+      );
 
-                setLoading(false)
-            });
-    }, [])
+      setLoading(false);
+    });
+  }, []);
 
-    if (loading) {
-        return "Loading..."
-    }
+  if (loading) {
+    return "Loading...";
+  }
 
-    return currencies.length ? (
-        <div className="currencies">
-            {
-                currencies.map((currency) => (
-                    <Currency
-                        key={currency.r030}
-                        imgUrl={`/flags/${currency.cc.toLowerCase()}.svg`}
-                        name={currency.txt}
-                        rate={currency.rate}
-                        code={currency.cc}
-                    />
-                ))
-            }
-        </div>
-    ) : (<p>No data</p >);
-
-}
+  return currencies.length ? (
+    <div className="currencies">
+      {currencies.map((currency) => (
+        <Currency
+          key={currency.r030}
+          imgUrl={`/flags/${currency.cc.toLowerCase()}.svg`}
+          name={currency.txt}
+          rate={currency.rate}
+          code={currency.cc}
+        />
+      ))}
+    </div>
+  ) : (
+    <p>No data</p>
+  );
+};
 
 export default CurrentExchangeRate;
